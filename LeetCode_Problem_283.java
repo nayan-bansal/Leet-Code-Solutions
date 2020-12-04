90593d44c88ecf71c58c5b494b590ab3d0131fa6
nayan-bansal <nayan.7991@gmail.com>
2020-12-04 23:59:31 +0530

    Added: Move Zeroes

---
+++ LeetCode_Problem_283.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0, k = nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] >0 || nums[i] <0){
+                nums[j] = nums[i];
+                j++;
+            }
+          
+        }
+        
+        for(int i = j;i<nums.length; i++)
+            nums[i] = 0;
+    }
+}
