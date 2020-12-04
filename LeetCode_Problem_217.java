4b3e7dbcc888fdb515dc283ebd07ddadcc5afe56
nayan-bansal <nayan.7991@gmail.com>
2020-12-04 21:32:57 +0530

    Added: Contains Duplicate

---
+++ LeetCode_Problem_217.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        
+        Arrays.sort(nums);
+        
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+           j = i+1;
+            if(nums[i] == nums[j])
+                return true;  
+        }
+        
+        return false;
+    }
+}
